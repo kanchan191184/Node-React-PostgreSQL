6f47bb7979eee548397676e4f719c69c7a50563a
kangarg <kanchan191184@gmail.com>
2021-07-15 13:37:49 +0530

    Code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [apiState, setapiState] = React.useState("");
+
+  const [usersState, setusersState] = React.useState("");
+
+  const [pgdbState, setpgdbState] = React.useState();
+
+  console.log("pgdb data" + pgdbState);
+
+  React.useEffect(() => {
+    fetch("http://localhost:9000/testAPI")
+      .then((res) => res.text())
+      .then((apiState) => setapiState(apiState));
+  }, []);
+
+  React.useEffect(
+    () => {
+      fetch("http://localhost:9000/users")
+        .then((res) => res.text())
+        .then((usersState) => setusersState(usersState));
+    },
+    // console.log("Hello" + usersState),
+    []
+  );
+
+  React.useEffect(() => {
+    fetch("http://localhost:9000/pgDatabase")
+      .then((res) => res.text())
+      .then((pgdbState) => setpgdbState(pgdbState));
+  }, []);
+
+  // const pgdb = pgdbState.map((i) => {
+  //   console.log("pgdb from client" + pgdb);
+  //   return <h1>{i.value}</h1>;
+  // });
+  console.log("Hello pgdbData from client" + pgdbState);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <div className="App">
+        <p>{apiState}</p>
+        <p>{usersState}</p>
+        <p>{pgdbState}</p>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+
+        {/* <ul>
+        {pgdbState.map((pgdb) => {
+          return (
+            <li key={pgdb.Id}>
+              <h3>{pgdb.Name}</h3>
+            </li>
+          );
+        })}
+      </ul> */}
+      </div>
+    </Fragment>
   );
 }
 
