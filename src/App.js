8e014112dfed7c379aaafee0f0ae8db7d8ed1703
kangarg <kanchan191184@gmail.com>
2021-07-20 08:08:33 +0530

    app.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,36 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  const [apiState, setapiState] = React.useState("");
+  const [apiState, setapiState] = useState("");
 
-  const [usersState, setusersState] = React.useState("");
+  const [usersState, setusersState] = useState("");
 
-  const [pgdbState, setpgdbState] = React.useState();
+  const [pgdbState, setpgdbState] = useState([]);
 
   console.log("pgdb data" + pgdbState);
 
-  React.useEffect(() => {
+  useEffect(() => {
     fetch("http://localhost:9000/testAPI")
       .then((res) => res.text())
       .then((apiState) => setapiState(apiState));
   }, []);
 
-  React.useEffect(
-    () => {
-      fetch("http://localhost:9000/users")
-        .then((res) => res.text())
-        .then((usersState) => setusersState(usersState));
-    },
-    // console.log("Hello" + usersState),
-    []
-  );
+  useEffect(() => {
+    fetch("http://localhost:9000/users")
+      .then((res) => res.text())
+      .then((usersState) => setusersState(usersState));
+  }, []);
 
-  React.useEffect(() => {
+  console.log("First Hello pgdbData from client" + pgdbState);
+
+  useEffect(() => {
     fetch("http://localhost:9000/pgDatabase")
       .then((res) => res.text())
       .then((pgdbState) => setpgdbState(pgdbState));
   }, []);
 
-  // const pgdb = pgdbState.map((i) => {
-  //   console.log("pgdb from client" + pgdb);
-  //   return <h1>{i.value}</h1>;
-  // });
   console.log("Hello pgdbData from client" + pgdbState);
 
   return (
@@ -45,19 +39,14 @@ function App() {
         <p>{apiState}</p>
         <p>{usersState}</p>
         <p>{pgdbState}</p>
+        {/* <ul>
+          {pgdbState.map(pgdb => (
+            <li key={pgdb.id}>{pgdb.name}</li>
+          ))}
+        </ul> */}
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-
-        {/* <ul>
-        {pgdbState.map((pgdb) => {
-          return (
-            <li key={pgdb.Id}>
-              <h3>{pgdb.Name}</h3>
-            </li>
-          );
-        })}
-      </ul> */}
       </div>
     </Fragment>
   );
